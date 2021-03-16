ad8bc5df387cd89c6fd89685c159c173db8a6675
Jorge Kania <jorgefkania@gmail.com>
2021-03-16 13:24:04 -0300

    Criação de função para jogar com o computador

--- js/script.js
+++ js/script.js
@@ -1,43 +1,91 @@
 'use strict';
 
+
+//constante e variáveis do jogo
 const celulas = document.querySelectorAll('.celula');
 const jogadorX = "✘";
 const jogadorO = "◎";
-
-let checarJogador = true;
+let fimDeJogo = false;
 let jogador;
+let centro;
+// let nivelJogo = 1;
+// let checarJogador = true;
+// let jogador_1 = document.getElementById('jogador_1').value;
+// let jogador_2 = document.getElementById('jogador_2').value;
 
+
+//Array com as combinações do tabuleiro
 const combinacoes = [
-    //Horizontais
+    //Array Linhas
     [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8],
 
-    //Verticais
+    //Array Colunas
     [0, 3, 6],
     [1, 4, 7],
     [2, 5, 8],
 
-    //Diagonais
+    //Array Diagonais
     [0, 4, 8],
     [2, 4, 6]
 ]
 
+//Detectar o clique na célula
 document.addEventListener('click', (event) => {
 
     if (event.target.matches('.celula')) {
-        jogar(event.target.id);
+        jogar(event.target.id, jogadorX);
+        setTimeout(() => jogadorCPU(), 1000);
     }
 })
 
-function jogar(id) {
+//Função para jogar com o computador 
+function jogadorCPU() {
+    const jogadasDisponiveis = [];
+    let index;
+
+    for (index in celulas) {
+        if (!isNaN(index)) {
+            if (
+                !celulas[index].classList.contains('✘') &&
+                !celulas[index].classList.contains('◎')) {
+                jogadasDisponiveis.push(index);
+            }
+        }
+    }
+
+    if (centro == false || centro == undefined) {
+        jogar(jogadasDisponiveis[4], jogadorO);
+        centro = true;
+    } else {
+        const jogadaAleatoria = Math.floor(
+            Math.random() * jogadasDisponiveis.length
+        );
+        if (!fimDeJogo) {
+            jogar(jogadasDisponiveis[jogadaAleatoria], jogadorO);
+        }
+    }
+
+}
+
+//Função para efetuar o jogo
+function jogar(id, jogador) {
     const celula = document.getElementById(id);
-    jogador = checarJogador ? jogadorX : jogadorO;
+    // jogador = checarJogador ? jogadorX : jogadorO;
+
+    if (celula.id == 4) {
+        centro = true;
+    }
+
     celula.textContent = jogador;
     celula.classList.add(jogador);
     analisarVencedor(jogador);
+
 }
 
+
+//Função que analisa se ouve um vencedor
 function analisarVencedor(jogador) {
     const vencedor = combinacoes.some((comb) => {
         return comb.every((index) => {
@@ -48,11 +96,13 @@ function analisarVencedor(jogador) {
         finalizarJogo(jogador);
     } else if (verificarEmpate()) {
         finalizarJogo();
-    } else {
-        checarJogador = !checarJogador;
     }
+    // else {
+    //     checarJogador = !checarJogador;
+    // }
 }
 
+//Função que analisa se teve empate
 function verificarEmpate() {
     let x = 0;
     let o = 0;
@@ -72,8 +122,11 @@ function verificarEmpate() {
     return x + o === 9 ? true : false;
 }
 
+
+//Função ara finalizar o jogo e mostrar a mensagem final
 function finalizarJogo(vencedor = null) {
 
+    fimDeJogo = true;
     const telaEscura = document.getElementById('tela-escura');
     const h2 = document.createElement('h2');
     const h3 = document.createElement('h3');
@@ -84,7 +137,7 @@ function finalizarJogo(vencedor = null) {
     telaEscura.appendChild(h3);
 
     if (vencedor) {
-        h2.innerHTML = `<img id="fogos" src="./img/fogos.gif"><br>O Jogador <span class="resultado">${vencedor}</span> venceu`;
+        h2.innerHTML = `<img id="fogos" src="./img/fogos.gif"><br><span class="resultado">${vencedor}</span> é o jogador vencedor`;
     } else {
         h2.innerHTML = `<img id="empate" src="./img/empate.gif"><br>O jogo Empatou.`;
     }
